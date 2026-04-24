8f424dc2b5f96981075285796a1f3e8b5f9c8c99
reece-stronge <rwstronge@outlook.com>
2026-04-24 01:34:34 +0100

    Update leg2_angle.m

--- MATLAB Kinematics Sim/leg2_angle.m
+++ MATLAB Kinematics Sim/leg2_angle.m
@@ -3,10 +3,10 @@ function angle2 = leg2_angle(n, h, L, L1, L2, L3)
         a2 = (L/(sqrt(n(1)^2+3*n(2)^2+4*n(3)^2+2*sqrt(3)*n(1)*n(2))))*(-n(3));
         b2 = (L/(sqrt(n(1)^2+3*n(2)^2+4*n(3)^2 +2*sqrt(3)*n(1)*n(2))))*(-sqrt(3)*n(3));
         c2 = h + (L/(sqrt(n(1)^2+3*n(2)^2+4*n(3)^2+2*sqrt(3)*n(1)*n(2))))*(sqrt(3)*n(2)+n(1));
-        B_m = [a2, b2, c2];
+        S2 = [a2, b2, c2];
         % Define Grouped Constants
-        A2 = -(B_m(1)+sqrt(3)*B_m(2)+2*L3)/B_m(3);
-        B2 = (B_m(1)^2+B_m(2)^2+B_m(3)^2+L2^2-L1^2-L3^2)/(2*B_m(3));
+        A2 = -(S2(1)+sqrt(3)*S2(2)+2*L3)/S2(3);
+        B2 = (S2(1)^2+S2(2)^2+S2(3)^2+L2^2-L1^2-L3^2)/(2*S2(3));
         C2 = A2^2+4;
         D2 = 2*A2*B2+4*L3;
         E2 = B2^2+L3^2-L2^2;
@@ -14,9 +14,9 @@ function angle2 = leg2_angle(n, h, L, L1, L2, L3)
         x2 = (-D2-sqrt(D2^2-4*C2*E2))/(2*C2);
         y2 = sqrt(3)*x2;
         z2 = sqrt(L2^2-4*x2^2-4*L3*x2-L3^2);
-        B_2 = [x2, y2, z2];
+        P2 = [x2, y2, z2];
         % Calculate Motor Angle
-        angle2 = 90 - rad2deg(atan2(sqrt(B_2(1)^2+B_2(2)^2)-L3, B_2(3)));
+        angle2 = 90 - rad2deg(atan2(sqrt(P2(1)^2+P2(2)^2)-L3, P2(3)));
 end
 
 
